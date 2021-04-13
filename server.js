9763b00f08f88ef9c7781df260b4456d3265ce37
Kimberly Kraemer <60149759+s1131610@users.noreply.github.com>
2021-04-13 19:41:38 -0400

    updated frontend server file with CORS adjustments

--- server.js
+++ server.js
@@ -2,13 +2,21 @@
 "use strict";
 const express = require("express");
 const compression = require("compression");
+const cors = require("cors");
 
 const _port = 80;
 const _app_folder = 'dist/my-vent';
 
 const app = express();
 app.use(compression());
+app.use(cors());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+    next();
+  });
 
 // ---- SERVE STATIC FILES ---- //
 app.get('*.*', express.static(_app_folder, {maxAge: '1y'}));
